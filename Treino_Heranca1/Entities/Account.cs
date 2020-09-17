b0e0428fd3b91605ed2e064a16a00aa72d442477
Raphael Scursoni <ra.scursoni@gmail.com>
2020-09-17 12:12:49 -0300

    Abstract class (Account) and your metods to stay efective

---
+++ Treino_Heranca1/Entities/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Treino_Heranca1.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //COMO POSSO FAZER UMA FUNÇÃO SER DIFERENTE PARA 
+        //AS CLASSES E SUBCLASSES?
+        //Basta na classe "Pai" colocar o -VIRTUAL- 
+        //Na classe "Filho" colocar -OVERRIDE- 
+        //Assim criando uma sobreposição.
+
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Treino_Heranca1/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Treino_Heranca1.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double IncreasedRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double increasedRate)
+            :base (number, holder, balance)
+        {
+            IncreasedRate = increasedRate;
+        }
+
+        public void UpdateBallance() {
+            Balance += Balance * IncreasedRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
